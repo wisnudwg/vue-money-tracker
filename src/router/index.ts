c049792aecc50963cb1be4f64cb28373704cc2d6
wisnubdd <wisnu@bolehdicoba.com>
2024-04-06 21:07:06 +0700

    added route guard using Authorization and Uid

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
+import { userStates } from '@/stores/user';
+const { id: { value: Uid }, token: { value: Authorization } } = userStates;
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (!['login', 'register'].includes(to.name as string) && (!Authorization || !Uid)) {
+    next({ name: 'login' });
+  } else {
+    next();
+  };
+});
 
 export default router
