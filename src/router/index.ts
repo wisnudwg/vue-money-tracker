22444bc0d3d4472eed3c05bbf302f4685e9f8e61
wisnubdd <wisnu@bolehdicoba.com>
2024-05-04 18:51:10 +0700

    added / route and redirect to login

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,9 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const { id: { value: Uid }, token: { value: Authorization } } = userStates;
+  if (to.path === "/") {
+    next({ name: "login" })
+  }
   if (!['login', 'register'].includes(to.name as string) && (!Authorization || !Uid)) {
     next({ name: 'login' });
   } /* else if (['login', 'register'].includes(to.name as string) && (Authorization && Uid)) {
